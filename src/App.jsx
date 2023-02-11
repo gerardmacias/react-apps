19286933a29709f6ab788b8127d9e33503db9b47
Gerard Macias <gerardo.maciascillero@amaris.com>
2023-02-11 19:32:24 +0100

    final_tv_show

--- src/App.jsx
+++ src/App.jsx
@@ -77,7 +77,7 @@ export function App() {
           <div className="col-4">
             <Logo
               image={logoImg}
-              title="Wath it"
+              title="Watch it"
               subtitle="Find a show you may like"
             />
           </div>
